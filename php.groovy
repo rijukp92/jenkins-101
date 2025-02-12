cacb3c8726045e532e91822ccb3537459101dceb
yagza@mail.ru <yagza@mail.ru>
2025-02-12 13:28:47 +0300

    groovy php v.1

---
+++ php.groovy
@@ -0,0 +1,24 @@
+timestamps {
+    ansiColor('xterm') {
+        node(env.JenkinsSlaveNode) {
+
+            env.PROJECT_NAME = "project_pso"
+            env.PROJECT_VERSION = "0.0.1"
+
+            try {
+                stage ("Git Checkout") {
+                    checkout scmGit(branches: [[name: GitBranchSource]], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkins-to-github', url: GitUrlSource]])
+                }
+            }
+
+            catch (exception) {
+                throw exception
+            }
+
+            finally {
+                println("Clean something")
+            }
+
+        }
+    }
+}
